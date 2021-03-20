fa036adf2c72d7330bc479b3c26bd6190f6dbdc0
rathersilly <rathersilly@live.com>
2021-03-19 23:49:41 -0600

    changed some user model methods to associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,31 @@ class User < ApplicationRecord
   #has_many :accepted_invites, class_name: 
   #has_many :invites, class_name: "Event", through: :invitations, foreign_key: :event_id, inverse_of: :invitee
   has_many :invited_events, through: :invites, source: :event
-  #has_many :accepted_events, through: :invites, -> { where accepted: true}, source: :event
+  has_many :invited_events, through: :invites, source: :event
+  has_many :accepted,  -> { where(accepted: true) },class_name: "Invitation", foreign_key: :invitee_id
+  has_many :pending,  -> { where(accepted: false) },class_name: "Invitation", foreign_key: :invitee_id
+  #has_many :accepted_events, -> { where(accepted: false) }through: :invites, -> { where accepted: true}, source: :event
+  has_many :accepted_events, through: :accepted, source: :event
+  has_many :pending_events, through: :pending, source: :event
+
 
   has_secure_password
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 4 }
   
   # can prob find a way to do these with has_many
-  def accepted
-    invites.select { |x| x.accepted == true }
-  end
-  def pending
-    invites.select { |x| x.accepted == false }
-  end
-  def accepted_events
-    accepted.each_with_object([]) { |x, a| a << Event.find(x.event_id) }
-  end
-  def pending_events
-    pending.each_with_object([]) { |x, a| a << Event.find(x.event_id) }
-  end
+  # yep!
+  # def accepted
+  #   invites.select { |x| x.accepted == true }
+  # end
+  # def pending
+  #   invites.select { |x| x.accepted == false }
+  # end
+  # def accepted_events
+  #   accepted.each_with_object([]) { |x, a| a << Event.find(x.event_id) }
+  # end
+  # def pending_events
+  #   pending.each_with_object([]) { |x, a| a << Event.find(x.event_id) }
+  # end
 
 end
